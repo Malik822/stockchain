855325cd48299fc9a6b7ab8a204abb1d8f5f5949
Malik822 <hallmalik22@gmail.com>
2018-04-02 22:25:52 -0400

    added the initialization, new block, new tranaction, hash, and last block functions

--- stockchain.py
+++ stockchain.py
@@ -1,3 +1,26 @@
 """Its Katie"""
 """Dan is in the house"""
 """Malik is aqui"""
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # creates a new Block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # adds a new transaction to the list of transactions
+        pass
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chian
+        pass
